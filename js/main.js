6c640832a7b9f3b358948e98fd2cf61479ae40d8
Vingnir <vingnir@live.se>
2022-01-18 07:51:40 +0100

    added js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function expand() {
+  var content = document.getElementById("content");
+  var heightContent = document.getElementById("content").offsetHeight;
+  if (heightContent == "600") {
+    content.style.height = "1000px";
+  }
+  else {
+    content.style.height = "600px";
+  }
+}
