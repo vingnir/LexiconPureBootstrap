75d0f89097069879ae03fd190e17f0c022b953e7
Vingnir <vingnir@live.se>
2022-01-20 11:29:31 +0100

    fixed navbar no js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
-      document.getElementById("navigation").classList.add("fixed-top");
-      // add padding top to show content behind navbar
-      navbar_height = document.querySelector(".navbar").offsetHeight;
-      document.body.style.paddingTop = navbar_height + "px";
-    } else {
-      document.getElementById("navigation").classList.remove("fixed-top");
-      // remove padding top from body
-      document.body.style.paddingTop = "0";
-    }
-  });
-});
+
 
 function expand() {
   var content = document.getElementById("content");
